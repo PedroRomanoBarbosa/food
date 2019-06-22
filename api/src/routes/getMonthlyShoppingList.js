205b0d9329e7c635bc6e7b871a54e083e7879a77
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2019-06-22 12:41:20 +0100

    Added pseudo random list as a function of month

--- api/src/routes/getMonthlyShoppingList.js
+++ api/src/routes/getMonthlyShoppingList.js
@@ -1,13 +1,21 @@
 import { getDB } from '../db';
+import seedrandom from 'seedrandom';
+import moment from 'moment';
 
 export async function getMonthlyShoppingList(req, res) {
   // TODO
-  res.send([
-    {
-      test: 'test',
-    },
-    {
-      test: 'test',
-    }
-  ]);
+  const db = await getDB();
+  const meals = await db.collection('meals').find({}).toArray();
+  const mealsNumber = meals.length;
+  const seed = moment(new Date()).format('YYYYMM');
+  var rng = seedrandom(seed);
+  var mealsList = [];
+  let counter = 0;
+  while(counter < mealsNumber) {
+    const index = Math.abs(rng.int32()) % (mealsNumber - counter);
+    mealsList.push(meals[index]);
+    meals.splice(index, 1);
+    counter += 1;
+  }
+  res.send(mealsList);
 }
