5f3d38253529a4f51adbe45016955cb87ea7174e
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2019-07-22 00:15:01 +0100

    Added insert meal and get all ingridients

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
-
 import {
   getMonthlyShoppingList,
   getMeals,
   getWeeklyShoppingList,
+  insertMeal,
+  getAllItems,
 } from './routes';
 
 async function initialize() {
   const app = express();
+  app.use(express.json());
   app.listen(4000, () =>
     console.log('Example app listening on port 4000!'),
   );
@@ -18,6 +20,8 @@ async function initialize() {
   app.get('/month-shopping-list', getMonthlyShoppingList);
   app.get('/weekly-shopping-list', getWeeklyShoppingList);
   app.get('/meals', getMeals);
+  app.get('/items', getAllItems);
+  app.post('/insertMeal', insertMeal);
 }
 
 initialize();
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,3 +1,5 @@
 export * from './getAllMeals';
 export * from './getMonthlyShoppingList';
 export * from './getWeeklyShoppingList';
+export * from './insertMeal';
+export * from './getAllItems';
