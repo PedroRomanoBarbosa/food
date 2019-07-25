ff2ecdb6aaec8522e5db4abbb02c3ab1c027c018
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2019-07-25 20:43:17 +0100

    Enabled CORS for the express server

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import {
   getMonthlyShoppingList,
   getMeals,
@@ -10,6 +11,7 @@ import {
 async function initialize() {
   const app = express();
   app.use(express.json());
+  app.use(cors());
   app.listen(4000, () =>
     console.log('Example app listening on port 4000!'),
   );
